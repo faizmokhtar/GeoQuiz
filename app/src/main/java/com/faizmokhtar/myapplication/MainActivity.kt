94ab0aa470d64b097a43e13f84772eaca50e5326
Faiz Mokhtar <mfmokhtar@gmail.com>
2022-02-08 17:41:01 +0800

    log lifecycle

--- app/src/main/java/com/faizmokhtar/myapplication/MainActivity.kt
+++ app/src/main/java/com/faizmokhtar/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.faizmokhtar.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
@@ -9,6 +10,7 @@ import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
@@ -27,9 +29,35 @@ class MainActivity : AppCompatActivity() {
     )
     private var currentIndex = 0
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // give Activity its UI
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
